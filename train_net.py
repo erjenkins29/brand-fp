c121e3da1ecf7c74368778cf48aba295a081b809
Evan <evan@earlydata.com>
2017-03-08 22:11:32 +0800

    first training script

---
+++ train_net.py
@@ -0,0 +1,70 @@
+#!/home/jupyter/py-env/py2.7.13/bin/python2.7
+
+import theano
+from theano import function, pp
+from theano import tensor as T
+
+## Define sigmoid layer
+def sigmoid_layer(x,w):
+   """
+   INPUT - 
+   x:  input vector  (numpy array)
+   w:  weight matrix (numpy 2darray)
+
+   OUTPUT -  element-wise sigmoid of...
+   [x (with an added bias term) times the weight matrix w]
+   """
+   b = np.array([1], dtype=theano.config.floatX)
+   x_with_bias = T.concatenate([x,b]) # attach the bias term to end of vector
+   z = T.dot(w.T, x_with_bias)
+   y = T.nnet.sigmoid(z)
+   return y
+
+## Define the output layer
+
+########### function is not clear right now.
+#### options:
+####   - log linear
+####   - linear
+####   - bins (log)
+
+## Define gradient descent function
+def grad_desc(cost, theta):
+   """
+   cost:  function which we are minimizing
+   theta: input vector
+   """
+   alpha = .1     #learning rate
+   return theta - (alpha * T.grad(cost,wrt=theta))
+
+## Declare variables
+
+## input vector
+x = theano.dvector('x')
+
+## weight matrix -- 
+##     input features = 30, outputs to a 8-node hidden layer
+theta1 = theano.shared(np.array(np.random.rand(30,8), dtype=theano.config.floatX))
+theta2 = theano.shared(np.array(np.random.rand(9,1), dtype=theano.config.floatX))
+
+
+### hidden layer
+h = sigmoid_layer(x, theta1)
+
+### output layer
+o = T.log(T.dot(h,theta2))
+
+## State cost function
+cost = (o - y)**2
+f_cost = theano.function([x], cost, updates=[
+     (theta1, grad_desc(cost, theta1)),
+     (theta2, grad_desc(cost, theta2))])
+f_overall = theano.function([x],o)
+
+# Read in the the training data here.
+
+
+## Output trained weights
+
+
+
